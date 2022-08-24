9f1641d6be2782ef3525f2a3e93cd9cb076eb415
hthant <hthant00chk@gmail.com>
2022-08-24 10:34:40 +0630

    created project sort-vis

--- Sort-visualizer/sketch.js
+++ Sort-visualizer/sketch.js
@@ -1,11 +1,30 @@
 const WIDTH = 500;
 const HEIGHT = 400;
-const  
+const MAXLENGTH = 100;
+const TOTAL = 50;
+let list;
 
 function setup() {
-	createCanvas(WIDTH, HEIGHT);
+  createCanvas(WIDTH, HEIGHT);
+  list = randList(TOTAL);
 }
 
 function draw() {
-	background(0);
+  background(0);
+  let x = 0;
+  let width = WIDTH / TOTAL;
+  for (let len in list) {
+    y = HEIGHT - (MAXLENGTH * len) / 1000;
+    fill(255);
+    rect(x * width, y, width, MAXLENGTH);
+  }
+}
+
+function randList(len) {
+  let arr = [];
+  for (let i = 0; i < len; i++) {
+    arr.push(random(1, 1000));
+  }
+  console.log(arr);
+  return arr;
 }
