90f8218ecfa55cebcddbf01ea45959d60658c2f3
hthant <hthant00chk@gmail.com>
2022-12-03 22:46:54 +0630

    Add key control

--- Sort-visualizer/sketch.js
+++ Sort-visualizer/sketch.js
@@ -1,30 +1,78 @@
-const WIDTH = 500;
-const HEIGHT = 400;
-const MAXLENGTH = 100;
-const TOTAL = 50;
+const WIDTH = 450;
+const HEIGHT = 300;
+const ARR_LEN = 300;
+const GAP = WIDTH / (ARR_LEN + 1);
+
 let list;
+let sortCounter;
+let clock;
+let sorting = false;
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-  list = randList(TOTAL);
+  newList();
 }
 
 function draw() {
   background(0);
   let x = 0;
-  let width = WIDTH / TOTAL;
-  for (let len in list) {
-    y = HEIGHT - (MAXLENGTH * len) / 1000;
-    fill(255);
-    rect(x * width, y, width, MAXLENGTH);
+  for (let len of list) {
+    x += GAP;
+    stroke(255);
+    line(x, HEIGHT, x, HEIGHT - len);
   }
+  if (!sorting) {
+    describe('Enter-continue; R-restart;', LABEL);
+    return;
+  }
+  let sorted = bubbleSort();
+
+  if (sorted) {
+    sorting = false;
+    describe(`Bubble Sort Complete! [${clock} ms]`, LABEL);
+  } else {
+    clock = parseInt(millis() - clock);
+    describe(`Bubble Sort in Process...[${clock} ms]`, LABEL);
+  }
+}
+
+function keyPressed() {
+  console.log(key);
+  if (key === 'Enter') {
+    sorting = !sorting;
+  } else if (!sorting && key === 'r') {
+    newList();
+  }
+}
+
+function newList() {
+  list = randList(ARR_LEN);
+  clock = millis();
+  sortCounter = 0;
 }
 
 function randList(len) {
   let arr = [];
   for (let i = 0; i < len; i++) {
-    arr.push(random(1, 1000));
+    arr.push(random(HEIGHT));
   }
-  console.log(arr);
   return arr;
+}
+
+function bubbleSort() {
+  if (sortCounter > list.length) return true;
+
+  for (let j = 0; j < list.length - 1 - sortCounter; j++) {
+    if (list[j] > list[j + 1]) {
+      swap(list, j, j + 1);
+    }
+  }
+  sortCounter++;
+  return false;
+}
+
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 }
